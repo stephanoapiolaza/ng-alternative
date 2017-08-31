51b02546c4382d0d29c3cf95f7a22e0fab733536
Stephano Apiolaza <stephano.apiolaza@imagemaker.cl>
2017-08-31 15:17:30 -0300

    chore(gulpfile): change name to target package json

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task('npm', function() {
   var targetPkgJson = {};
   var fieldsToCopy = ['version', 'description', 'keywords', 'author', 'repository', 'license', 'bugs', 'homepage'];
 
-  targetPkgJson['name'] = '@ng-bootstrap/ng-bootstrap';
+  targetPkgJson['name'] = '@stephanoapiolaza/ng-alternative';
 
   fieldsToCopy.forEach(function(field) { targetPkgJson[field] = pkgJson[field]; });
 
